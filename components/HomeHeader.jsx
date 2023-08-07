3c56e821ffa7638885cc39b8b60c7e578c6ea346
shm_usuf <shmusuf@gmail.com>
2023-08-07 12:13:41 +0600

    home header design added

--- components/HomeHeader.jsx
+++ components/HomeHeader.jsx
@@ -3,12 +3,18 @@ import { TbGridDots } from "react-icons/tb";
 
 const HomeHeader = () => {
   return (
-    <header>
-      <div className="">
-        <Link href="#">Gmail</Link>
-        <Link href="#">Image</Link>
-        <TbGridDots />
-        <button>Sign In</button>
+    <header className="flex justify-end p-4">
+      <div className="flex items-center space-x-4 text-sm">
+        <Link href="#" className="hover:underline cursor-pointer duration-300">
+          Gmail
+        </Link>
+        <Link href="#" className="hover:underline cursor-pointer duration-300">
+          Image
+        </Link>
+        <TbGridDots className="text-4xl hover:bg-slate-200/95 rounded-full bg-transparent p-2 cursor-pointer duration-300" />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md transition-all hover:brightness-110">
+          Sign In
+        </button>
       </div>
     </header>
   );
