d1589e424e7a29336fdf1db6fbc7e08820c4dca7
shm_usuf <shmusuf@gmail.com>
2023-08-07 16:07:15 +0600

    form design added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,24 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
     <>
       <HomeHeader />
+      {/* BODY */}
+      <main className="flex flex-col items-center gap-10">
+        <div className="flex justify-center items-center mt-24">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+            width={300}
+            height={50}
+            alt="google image"
+            className="w-56 sm:w-64"
+          />
+        </div>
+        <HomeSearch />
+      </main>
     </>
   );
 };
---
+++ components/HomeSearch.jsx
@@ -0,0 +1,19 @@
+import { BiSearchAlt2 } from "react-icons/bi";
+import { HiMicrophone } from "react-icons/hi";
+const HomeSearch = () => {
+  return (
+    <>
+      <form className="flex items-center w-full mx-auto max-w-[90%] border border-gray-200 px-6 py-3 gap-3 rounded-full hover:shadow-md transition-shadow focus-within:shadow-md sm:max-w-lg lg:max-w-2xl">
+        <BiSearchAlt2 className="text-xl text-gray-500" />
+        <input
+          type="text"
+          placeholder="Search Google or type a URL"
+          className="w-full focus:outline-none placeholder:text-xs sm:placeholder:text-base"
+        />
+        <HiMicrophone className="text-xl" />
+      </form>
+    </>
+  );
+};
+
+export default HomeSearch;
