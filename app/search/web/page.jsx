8705e03eaf4622504f91cf7f8d243730bbd902e7
shm_usuf <shmusuf@gmail.com>
2023-08-10 11:25:50 +0600

    data fetch from google search api

--- app/search/web/page.jsx
+++ app/search/web/page.jsx
@@ -1,5 +1,19 @@
-const WebSearchPage = () => {
-  return <div>WebSearchPage</div>;
+const WebSearchPage = async ({ searchParams }) => {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await res.json();
+  const results = data.items;
+  return (
+    <>
+      {results &&
+        results.map((result) => (
+          <div key={result.cacheId}>
+            <h3>{result.title}</h3>
+          </div>
+        ))}
+    </>
+  );
 };
 
 export default WebSearchPage;
