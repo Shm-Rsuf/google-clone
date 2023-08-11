208e1a23a66906ebf05addaca14018177f00285d
shm_usuf <shmusuf@gmail.com>
2023-08-11 22:08:28 +0600

    add loading for web and image

--- app/search/image/loading.jsx
+++ app/search/image/loading.jsx
@@ -1,5 +1,25 @@
 const loading = () => {
-  return <div>loading...</div>;
+  return (
+    <div className="mt-5 px-2 lg:ml-52 max-w-6xl flex gap-4 flex-col sm:flex-row pb-20">
+      <div className="animate-pulse">
+        <div className="h-48 w-48 mb-4 bg-gray-200 rounded-md "></div>
+        <div className="h-3 w-48 mb-2.5 bg-gray-200 rounded-md "></div>
+        <div className="h-2 w-44 mb-2 bg-gray-200 rounded-md "></div>
+      </div>
+      <div className="hidden sm:inline-flex gap-4">
+        <div className="animate-pulse">
+          <div className="h-48 w-48 mb-4 bg-gray-200 rounded-md "></div>
+          <div className="h-3 w-48 mb-2.5 bg-gray-200 rounded-md "></div>
+          <div className="h-2 w-44 mb-2 bg-gray-200 rounded-md "></div>
+        </div>
+        <div className="animate-pulse">
+          <div className="h-48 w-48 mb-4 bg-gray-200 rounded-md "></div>
+          <div className="h-3 w-48 mb-2.5 bg-gray-200 rounded-md "></div>
+          <div className="h-2 w-44 mb-2 bg-gray-200 rounded-md "></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default loading;
